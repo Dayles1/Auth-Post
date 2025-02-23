169aae034a42538a45feab36c9beb438a69a5623
Dayles1 <sololeveling0025@gmail.com>
2025-02-23 16:10:15 +0500

    +LoginRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         $user =User::create([
             'name'=>$request->name,
@@ -25,7 +26,7 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        
+
 
     }
 }
