7287d7c8d31d99ee4208aeec1a5113b176f93e6c
Dayles1 <sololeveling0025@gmail.com>
2025-02-23 19:42:55 +0500

    +CommentRequest

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCommentRequest;
 
 class CommentController extends Controller
 {
   
+public function addComment(StoreCommentRequest $request,$id){
 
+    $comment=Comment::create([
+        'comment'
+    ]);
+
+}
+
+public function deleteComment(){
+
+}
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::post('/register',[AuthController::class, 'register']);
+    Route::post('posts', [PostController::class, 'store']);
+    Route::get('posts', [PostController::class, 'index']);
+    Route::get('posts/{post}', [PostController::class, 'show']);
+    
+    Route::put('posts/{post}', [PostController::class, 'update'])->middleware('check.post.owner');
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->middleware('check.post.owner');
+
+    Route::post('comments', [CommentController::class, 'addComment']);
+    Route::delete('comments/{comment}', [CommentController::class, 'deleteComment']);
+
+    Route::post('logout', [AuthController::class, 'logout']);
+});
